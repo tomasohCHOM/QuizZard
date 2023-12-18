ed40d2f5c93aa99788262e60ae0e0a0e9b1eede0
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2023-12-18 15:57:41 -0800

    Corroborated correct/incorrect answers

--- src/lib/components/question.svelte
+++ src/lib/components/question.svelte
@@ -3,12 +3,23 @@
 
 	const question = "Find the square root of 16";
 
+	interface Answer {
+		answer: String;
+		correct: boolean;
+	}
+
 	const answers = [
-		"this is an answer",
-		"this is an answer",
-		"this is an answer",
-		"this is an answer"
+		{ answer: "this is an answer", correct: true },
+		{ answer: "this is an answer", correct: false },
+		{ answer: "this is an answer", correct: false },
+		{ answer: "this is an answer", correct: false }
 	];
+
+	let answerSelected: boolean = false;
+
+	function answerClicked() {
+		answerSelected = true;
+	}
 </script>
 
 <section class="p-10 md:p-20">
@@ -19,9 +30,18 @@
 		<p>{question}</p>
 	</div>
 
-	<div class="grid grid-cols-1 items-center justify-center gap-4 md:gap-8 sm:grid-cols-2">
+	<div class="grid grid-cols-1 items-center justify-center gap-4 sm:grid-cols-2 md:gap-8">
 		{#each answers as answer}
-			<div class="cursor-pointer rounded-xl border-2 border-slate-700 p-4">{answer}</div>
+			<button
+				class="cursor-pointer rounded-xl border-2 border-slate-700 p-4 transition hover:opacity-80 {answerSelected
+					? answer.correct
+						? 'bg-green-700'
+						: 'bg-red-700'
+					: ''} {answerSelected ? 'border-none text-slate-50' : ''}"
+				on:click={answerClicked}
+			>
+				{answer.answer}
+			</button>
 		{/each}
 	</div>
 </section>
