aebf09f783d5aa4e7dacf8760c809646afa267fb
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2023-12-26 15:03:07 -0800

    Added Supabase db to project

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,10 +1,16 @@
-// See https://kit.svelte.dev/docs/types#app
+import type { Session, SupabaseClient } from "@supabase/supabase-js";
+
 // for information about these interfaces
 declare global {
 	namespace App {
+		interface Locals {
+			supabase: SupabaseClient;
+			getSession(): Promise<Session | null>;
+		}
+		interface PageData {
+			session: Session | null;
+		}
 		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
 	}
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,4 +1,35 @@
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from "$env/static/public";
 import type { Handle } from "@sveltejs/kit";
+import { createServerClient } from "@supabase/ssr";
+import { sequence } from "@sveltejs/kit/hooks";
+
+export const createClient: Handle = async ({ event, resolve }) => {
+	event.locals.supabase = createServerClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
+		cookies: {
+			get: (key) => event.cookies.get(key),
+			set: (key, value, options) => event.cookies.set(key, value, options as any),
+			remove: (key, options) => event.cookies.delete(key, options as any)
+		}
+	});
+
+	/**
+	 * a little helper that is written for convenience so that instead
+	 * of calling `const { data: { session } } = await supabase.auth.getSession()`
+	 * you just call this `await getSession()`
+	 */
+	event.locals.getSession = async () => {
+		const {
+			data: { session }
+		} = await event.locals.supabase.auth.getSession();
+		return session;
+	};
+
+	return resolve(event, {
+		filterSerializedResponseHeaders(name) {
+			return name === "content-range";
+		}
+	});
+};
 
 const handleTheme = (async ({ event, resolve }) => {
 	const theme = event.cookies.get("theme");
@@ -11,4 +42,4 @@ const handleTheme = (async ({ event, resolve }) => {
 	return await resolve(event);
 }) satisfies Handle;
 
-export const handle = handleTheme;
+export const handle = sequence(createClient, handleTheme);
