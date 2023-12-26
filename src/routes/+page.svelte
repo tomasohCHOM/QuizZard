efaf97b55c7303eb1963a4d720b2f32ddd5f90d7
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2023-12-26 15:14:23 -0800

    Added /quiz route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,8 @@
-<script lang="ts">
-	import "../app.css";
-	import QUESTION_LIST from "$lib/questions/sample";
-	import QuestionSet from "$lib/components/question-set.svelte";
-
-	const RANDOMIZED_ANSWER_LIST = QUESTION_LIST.map((question) => {
-		const answers = [question.correctAnswer, ...question.incorrectAnswers].sort(
-			() => Math.random() - 0.5
-		);
-		return { ...question, answers };
-	});
-</script>
-
-<QuestionSet questionSet={RANDOMIZED_ANSWER_LIST} />
+<div class="p-10">
+	<a
+		class="rounded border-contrast bg-secondary p-4 shadow-sm transition hover:opacity-80 group-data-[theme='dark']:border-2"
+		href="/quiz"
+	>
+		Go to Quiz!
+	</a>
+</div>
---
+++ src/routes/quiz/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import "../../app.css";
+	import QUESTION_LIST from "$lib/questions/sample";
+	import QuestionSet from "$lib/components/question-set.svelte";
+
+	const RANDOMIZED_ANSWER_LIST = QUESTION_LIST.map((question) => {
+		const answers = [question.correctAnswer, ...question.incorrectAnswers].sort(
+			() => Math.random() - 0.5
+		);
+		return { ...question, answers };
+	});
+</script>
+
+<QuestionSet questionSet={RANDOMIZED_ANSWER_LIST} />
