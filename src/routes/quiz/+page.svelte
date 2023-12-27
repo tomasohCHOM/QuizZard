e3ec4d3c37fe7734b3956013f4664c88a6f1c732
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2023-12-27 14:39:56 -0800

    Moved QuestionSet to routes/quiz/+page.svelte

--- src/routes/quiz/+page.svelte
+++ src/routes/quiz/+page.svelte
@@ -1,14 +1,60 @@
 <script lang="ts">
 	import "../../app.css";
+  import type { QuestionSet } from "$lib/shared";
 	import QUESTION_LIST from "$lib/questions/sample";
-	import QuestionSet from "$lib/components/question-set.svelte";
+	import Question from "$lib/components/question.svelte";
+	import ProgressBar from "$lib/components/progress-bar.svelte";
 
-	const RANDOMIZED_ANSWER_LIST = QUESTION_LIST.map((question) => {
+	const questionSet: QuestionSet[] = QUESTION_LIST.map((question) => {
 		const answers = [question.correctAnswer, ...question.incorrectAnswers].sort(
 			() => Math.random() - 0.5
 		);
 		return { ...question, answers };
 	});
+
+  
+	let questionNumber = 0;
+	let answerSelected = false;
+	let correctAnswers: boolean[] | null[] = new Array(questionSet.length).fill(null);
 </script>
 
-<QuestionSet questionSet={RANDOMIZED_ANSWER_LIST} />
+
+<div class="p-10 md:p-20">
+	<Question
+		{questionNumber}
+		questionPrompt={questionSet[questionNumber].prompt}
+		answers={questionSet[questionNumber].answers}
+		correct={questionSet[questionNumber].correctAnswer}
+		bind:answerSelected
+		bind:correctAnswers
+	>
+		<div class="mt-4 flex flex-row-reverse justify-between" slot="buttons">
+			{#if questionNumber === questionSet.length - 1}
+				<button
+					class="rounded-lg border-2 border-contrast px-6 py-3 font-semibold text-contrast transition hover:bg-secondary"
+					>Finish</button
+				>
+			{/if}
+			{#if questionNumber < questionSet.length - 1}
+				<button
+					class="rounded-lg border-2 border-contrast px-6 py-3 font-semibold text-contrast transition hover:bg-secondary"
+					on:click={() => {
+						questionNumber++;
+						answerSelected = false;
+					}}>Next</button
+				>
+			{/if}
+			{#if questionNumber > 0}
+				<button
+					class="rounded-lg border-2 border-contrast px-6 py-3 font-semibold text-contrast transition hover:bg-secondary"
+					on:click={() => {
+						questionNumber--;
+						answerSelected = false;
+					}}>Previous</button
+				>
+			{/if}
+		</div>
+	</Question>
+
+	<ProgressBar bind:correctAnswers />
+</div>
