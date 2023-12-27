47d89497bbd061e62dc93ffbdc6988324ff50755
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2023-12-27 15:52:29 -0800

    Starting Finished Quiz section

--- src/routes/quiz/+page.svelte
+++ src/routes/quiz/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import "../../app.css";
-  import type { QuestionSet } from "$lib/shared";
+	import type { QuestionSet } from "$lib/shared";
 	import QUESTION_LIST from "$lib/questions/sample";
 	import Question from "$lib/components/question.svelte";
 	import ProgressBar from "$lib/components/progress-bar.svelte";
@@ -12,49 +12,65 @@
 		return { ...question, answers };
 	});
 
-  
 	let questionNumber = 0;
 	let answerSelected = false;
 	let correctAnswers: boolean[] | null[] = new Array(questionSet.length).fill(null);
+	let correctAnswerCount = 0;
+
+	// Represents whether the user is done with the quiz or not
+	let isFinished = false;
+
+	function calculateFinalResults() {
+		isFinished = true;
+		correctAnswers.forEach((isCorrect) => {
+			if (isCorrect) correctAnswerCount++;
+		});
+	}
 </script>
 
-
 <div class="p-10 md:p-20">
-	<Question
-		{questionNumber}
-		questionPrompt={questionSet[questionNumber].prompt}
-		answers={questionSet[questionNumber].answers}
-		correct={questionSet[questionNumber].correctAnswer}
-		bind:answerSelected
-		bind:correctAnswers
-	>
-		<div class="mt-4 flex flex-row-reverse justify-between" slot="buttons">
-			{#if questionNumber === questionSet.length - 1}
-				<button
-					class="rounded-lg border-2 border-contrast px-6 py-3 font-semibold text-contrast transition hover:bg-secondary"
-					>Finish</button
-				>
-			{/if}
-			{#if questionNumber < questionSet.length - 1}
-				<button
-					class="rounded-lg border-2 border-contrast px-6 py-3 font-semibold text-contrast transition hover:bg-secondary"
-					on:click={() => {
-						questionNumber++;
-						answerSelected = false;
-					}}>Next</button
-				>
-			{/if}
-			{#if questionNumber > 0}
-				<button
-					class="rounded-lg border-2 border-contrast px-6 py-3 font-semibold text-contrast transition hover:bg-secondary"
-					on:click={() => {
-						questionNumber--;
-						answerSelected = false;
-					}}>Previous</button
-				>
-			{/if}
+	{#if isFinished}
+		<div class="flex gap-4 rounded-lg border-contrast p-4 group-data-[theme='dark']:border-2">
+			<div class="h-36 w-36 rounded-[50%] bg-contrast" />
+			<div>{correctAnswerCount} out of {questionSet.length} correct</div>
 		</div>
-	</Question>
+	{:else}
+		<Question
+			{questionNumber}
+			questionPrompt={questionSet[questionNumber].prompt}
+			answers={questionSet[questionNumber].answers}
+			correct={questionSet[questionNumber].correctAnswer}
+			bind:answerSelected
+			bind:correctAnswers
+		>
+			<div class="mt-4 flex flex-row-reverse justify-between" slot="buttons">
+				{#if questionNumber === questionSet.length - 1}
+					<button
+						class="rounded-lg border-2 border-contrast px-6 py-3 font-semibold text-contrast transition hover:bg-secondary"
+						on:click={calculateFinalResults}>Finish</button
+					>
+				{/if}
+				{#if questionNumber < questionSet.length - 1}
+					<button
+						class="rounded-lg border-2 border-contrast px-6 py-3 font-semibold text-contrast transition hover:bg-secondary"
+						on:click={() => {
+							questionNumber++;
+							answerSelected = false;
+						}}>Next</button
+					>
+				{/if}
+				{#if questionNumber > 0}
+					<button
+						class="rounded-lg border-2 border-contrast px-6 py-3 font-semibold text-contrast transition hover:bg-secondary"
+						on:click={() => {
+							questionNumber--;
+							answerSelected = false;
+						}}>Previous</button
+					>
+				{/if}
+			</div></Question
+		>
 
-	<ProgressBar bind:correctAnswers />
+		<ProgressBar bind:correctAnswers />
+	{/if}
 </div>
