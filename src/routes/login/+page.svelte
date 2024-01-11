826ecda59e7eedc1df9067143d0dc8f695961750
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-01-11 12:21:19 -0800

    Styled login route

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,17 +1,35 @@
 <script lang="ts">
+	import Icon from "@iconify/svelte";
 	import type { PageData } from "./$types";
 
 	export let data: PageData;
 	$: ({ supabase } = data);
 </script>
 
-<h2>Login</h2>
-
 <svelte:head>
 	<title>Login | QuizZard</title>
 </svelte:head>
 
-<form method="post">
-	<button formaction="?/login&provider=github">Github</button>
-	<button formaction="?/login&provider=discord">Discord</button>
-</form>
+<section class="flex flex-col items-center">
+	<h2 class="text-2xl font-semibold md:text-3xl">Login</h2>
+
+	<form
+		method="post"
+		class="mt-8 flex w-[min(30rem,_90vw)] flex-col items-center justify-center gap-4 rounded-xl border-2 border-contrast p-6"
+	>
+		<button
+			formaction="?/login&provider=github"
+			class="flex w-full items-center justify-center gap-4 rounded-lg border-2 border-contrast py-2 transition group-data-[theme='light']:hover:opacity-85 group-data-[theme='dark']:hover:brightness-110"
+		>
+			<Icon class="inline-block" width={40} inline={true} icon="mdi:github" />
+			<span class="align-middle font-medium">Sign in with GitHub</span>
+		</button>
+		<button
+			formaction="?/login&provider=discord"
+			class="flex w-full items-center justify-center gap-4 rounded-lg border-2 border-contrast py-2 transition group-data-[theme='light']:hover:opacity-85 group-data-[theme='dark']:hover:brightness-110"
+		>
+			<Icon class="inline-block" width={40} inline={true} icon="ic:baseline-discord" />
+			<span class="align-middle font-medium">Sign in with Discord</span>
+		</button>
+	</form>
+</section>
