813e6add745f35b61ee4586029e15a449fcb3ff6
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-01-11 14:08:32 -0800

    Added /create route and started implementation of Quiz creation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,36 @@
 <script lang="ts">
+	import QuizSet from "$lib/components/quiz-set.svelte";
+	import Icon from "@iconify/svelte";
 	import type { PageData } from "./$types";
 
 	export let data: PageData;
+
+	let quizzes: QuizSet[] = data.quizzes;
 </script>
 
-{#if data.session}
-	Im logged in
-	{data.quizzes}
-{/if}
-<section class="flex flex-col gap-8">
-	<h2 class="text-2xl font-semibold md:text-3xl">Home</h2>
+<section class="flex flex-col gap-6">
+	<h1 class="text-3xl font-semibold md:text-4xl">Home</h1>
 
-	<div>
+	<h2 class="text-xl font-medium md:text-3xl">Recent Quizzes</h2>
+
+	<a
+		href="/quiz"
+		class="block w-64 cursor-pointer rounded border-contrast bg-secondary p-4 font-medium shadow-md transition group-data-[theme='dark']:border-2 group-data-[theme='light']:hover:opacity-85 group-data-[theme='dark']:hover:brightness-110"
+	>
+		ACM Quiz
+	</a>
+
+	{#if data.session}
+		<h2 class="text-x; font-medium md:text-3xl">Your Quizzes</h2>
+		{#if quizzes.length === 0}
+			<p>You have no quizzes created at this time.</p>
+		{/if}
 		<a
-			class="rounded border-contrast bg-secondary p-4 shadow-md transition group-data-[theme='dark']:border-2 group-data-[theme='light']:hover:opacity-85 group-data-[theme='dark']:hover:brightness-110"
-			href="/quiz"
+			href="/create"
+			class="flex w-max items-center gap-1 rounded-md bg-secondary py-2 pr-4 transition group-data-[theme='dark']:border-2 group-data-[theme='light']:hover:opacity-85 group-data-[theme='dark']:hover:brightness-110"
 		>
-			Go to Quiz!
+			<Icon class="inline-block" width={40} inline={true} icon="ic:baseline-plus" />
+			<span class="align-middle font-medium">Create a Quiz!</span>
 		</a>
-	</div>
+	{/if}
 </section>
