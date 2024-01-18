1de3aec85c0106b5510bda15a8fdb8358e933fe3
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-01-18 14:24:23 -0800

    Removed form action for now

--- src/routes/create/+page.server.ts
+++ src/routes/create/+page.server.ts
@@ -1,10 +1,5 @@
 import type { Actions } from "@sveltejs/kit";
-import QUESTION_SET from "$lib/questions/sample";
 
 export const actions: Actions = {
-	default: async ({ locals: { supabase } }) => {
-		const res = await supabase
-			.from("quiz")
-			.insert({ name: "ACM Quiz", question_set: QUESTION_SET });
-	}
+	default: async ({ locals: { supabase } }) => {}
 };
