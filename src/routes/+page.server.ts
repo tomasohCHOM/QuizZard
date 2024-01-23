93dc12e8996e29ff6675b077724dd4227f6b5908
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-01-23 12:13:16 -0800

    Fixed quiz return type at root page

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,18 +1,16 @@
 import type { QuestionSet } from "$lib/shared";
+import { error } from "@sveltejs/kit";
 import type { Tables } from "../lib/db/types";
 import type { PageServerLoad } from "./$types";
 
-interface QuizReturnType {
-	quizzes: {
-		question_set: QuestionSet;
-	};
-}
+type QuizReturnType = Tables<"quiz"> & { question_set: QuestionSet[] };
 
 export const load: PageServerLoad = async ({ locals: { supabase } }) => {
-	const quizzes = await supabase
-		.from("quiz")
-		.select("")
-		.returns<Tables<"quiz" & QuizReturnType>[]>();
+	const { data, error: err } = await supabase.from("quiz").select("").returns<QuizReturnType[]>();
 
-	return { quizzes: quizzes.data ?? [] };
+	if (err) {
+		error(500, "Server error");
+	}
+
+	return { quizzes: data ?? [] };
 };
--- src/routes/quiz/[id]/+page.server.ts
+++ src/routes/quiz/[id]/+page.server.ts
@@ -2,9 +2,9 @@ import type { QuestionSet } from "$lib/shared";
 import type { PageServerLoad } from "./$types";
 import { error } from "@sveltejs/kit";
 
-interface QuestionSetReturnType {
+type QuestionSetReturnType = {
 	question_set: QuestionSet[];
-}
+};
 
 export const load: PageServerLoad = async ({ params, locals: { supabase } }) => {
 	const { data, error: err } = await supabase
