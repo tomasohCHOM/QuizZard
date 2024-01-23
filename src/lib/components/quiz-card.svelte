8ebe78a0009bb08d7e7c83b2e1fb325aa8ff6c71
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-01-23 15:43:12 -0800

    Updated QuizCard

--- src/lib/components/quiz-card.svelte
+++ src/lib/components/quiz-card.svelte
@@ -7,9 +7,13 @@
 <div>
 	<a
 		href="/quiz/{quizId}"
-		class="block cursor-pointer rounded border-contrast bg-secondary p-4 font-medium shadow-md transition group-data-[theme='dark']:border-2 group-data-[theme='light']:hover:opacity-85 group-data-[theme='dark']:hover:brightness-110"
+		class="flex cursor-pointer flex-col gap-6 rounded border-contrast bg-secondary p-4 font-medium shadow-md transition group-data-[theme='dark']:border-2 group-data-[theme='light']:hover:opacity-85 group-data-[theme='dark']:hover:brightness-110"
 	>
-		{quizName}
-		{quizLength} questions
+		<span class="border-b-2 border-contrast py-2 font-semibold">
+			{quizName}
+		</span>
+		<span>
+			<span class="font-semibold">{quizLength}</span> questions
+		</span>
 	</a>
 </div>
