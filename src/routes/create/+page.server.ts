567e4c947d04ca4722db878c1f523c39c41960f6
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-02-02 18:45:11 -0800

    Created set up code for submitting quizzes

--- src/routes/create/+page.server.ts
+++ src/routes/create/+page.server.ts
@@ -1,8 +1,16 @@
 import sample from "$lib/questions/sample";
-import type { Actions } from "@sveltejs/kit";
+import { error, type Actions } from "@sveltejs/kit";
 
 export const actions: Actions = {
-	default: async ({ locals: { supabase } }) => {
-		// await supabase.from("quiz").insert({ name: "MATH 251A-01 10.1 Quiz", question_set: sample });
+	default: async ({ locals: { supabase, getSession } }) => {
+		const session = await getSession();
+
+		if (!session) {
+			error(404, "Not found");
+		}
+
+		await supabase
+			.from("quiz")
+			.insert({ name: "MATH 251A-01 10.4 Quiz", question_set: sample, user_id: session.user.id });
 	}
 };
