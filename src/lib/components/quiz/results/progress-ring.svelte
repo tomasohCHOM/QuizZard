839742a01ea4e69f806c1b718ed6226acc27effc
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-05-17 10:44:13 -0700

    Fix: buggy ring animation delay

--- src/lib/components/quiz/results/progress-ring.svelte
+++ src/lib/components/quiz/results/progress-ring.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	export let percentage: number;
+	let ringOffset = 251.2 - (251.2 * percentage) / 100;
 </script>
 
 <svg class="relative h-32 w-32" viewBox="0 0 100 100">
@@ -18,7 +19,7 @@
 		fill="transparent"
 		stroke-width="5"
 		class="progress-ring-circle stroke-current text-green-500"
-		style="stroke-dashoffset: calc(251.2 - (251.2 * {percentage}) / 100);"
+		style="--ring-offset: {ringOffset}"
 	/>
 	<text
 		x="50"
@@ -36,14 +37,19 @@
 	.progress-ring-circle {
 		stroke-linecap: round;
 		stroke-dasharray: 251.2;
+		stroke-dashoffset: 251.2;
 		transform: rotate(-90deg);
 		transform-origin: 50% 50%;
-		animation: progress 1s ease forwards;
+		animation: progress 750ms ease forwards;
+		animation-delay: 300ms;
 	}
 
 	@keyframes progress {
 		from {
 			stroke-dashoffset: 251.2;
+		}
+		to {
+			stroke-dashoffset: var(--ring-offset);
 		}
 	}
 </style>
