ea81ea912217b03fb6063324558c08fb958c4ba5
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-05-28 13:58:04 -0700

    Add basic error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { page } from "$app/stores";
+</script>
+
+<section class="grid items-center justify-center gap-4">
+	<h1 class="text-center text-3xl">
+		<span class="font-bold">{$page.status}</span> |
+		<span class="font-medium">
+			{$page.error?.message}
+		</span>
+	</h1>
+	<div class="flex gap-2">
+		<a href="/" class="quiz-btn w-max">Go Back Home</a>
+		<a href="/quizzes" class="quiz-btn-contrast w-max">Explore Quizzes</a>
+	</div>
+</section>
