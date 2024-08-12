14cc9153a22931b01748e16187e700cfeada408e
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-08-12 13:14:06 -0700

    Create privacy policy

---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,33 @@
+<section class="flex max-w-screen-lg flex-col gap-6 p-4">
+	<h1 class="text-2xl font-semibold md:text-4xl">Quizzard: Privacy Policy</h1>
+	<p><strong>Effective Date:</strong> August 12, 2024</p>
+	<p>
+		Your privacy is important to us. This policy explains how we handle your information when you
+		use Quizzard.
+	</p>
+
+	<h2 class="text-xl font-semibold md:text-2xl">1. Information We Collect</h2>
+	<ul class="ml-6 list-disc">
+		<li>
+			We use Google, GitHub, and Discord OAuth for authentication purposes. We only collect the
+			necessary information required to authenticate you and do not use the information provided for
+			any other purposes.
+		</li>
+	</ul>
+
+	<h2 class="text-xl font-semibold md:text-2xl">2. How We Use Your Information</h2>
+	<ul class="ml-6 list-disc">
+		<li>
+			<strong>Authentication:</strong> We use your OAuth information solely to authenticate your identity
+			when you log in to Quizzard.
+		</li>
+	</ul>
+
+	<h2 class="text-xl font-semibold md:text-2xl">2. The Information We Share</h2>
+	<ul class="ml-6 list-disc">
+		<li>We do not share your authentication information with any third parties.</li>
+	</ul>
+
+	<h2 class="text-xl font-semibold md:text-2xl">3. Contact Us</h2>
+	<p>Questions? Reach out at tomasoh@csu.fulllerton.edu.</p>
+</section>
